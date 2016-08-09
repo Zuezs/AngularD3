7759b0981a48f61dc913cbaaa26a640295ee731f
Zuezs <tallenbusiness@gmail.com>
2016-08-09 14:34:27 -0500

    Adding Controller Functionality
    Controller Functionality

--- app/view8/view8.js
+++ app/view8/view8.js
@@ -7,16 +7,45 @@ angular.module('myApp.view8', ['ngRoute'])
         });
     }])
 
-    .controller('View8Ctrl', [function() {
+    .controller('View8Ctrl', ['$scope', 'DataFactory', function($scope, DataFactory) {
 
+        $scope.data = [];
+        $scope.get = function () {
+            $scope.items = DataFactory.getCharacters();
+            //the model returns a promise and THEN items
+            $scope.items.then(function (items) {
+                 console.log(JSON.stringify(items.data));
+                 console.log(items.data);
+                 return this.$scope.data = items.data;
+            }, function (status) {
+                console.log(status);
+            })
+        }
     }])
 
-    .service('DataService', function () {
+    .factory('DataFactory', ['$http', '$q' , function ($http, $q) {
 
-    })
+       return {
+           getCharacters: function () {
+               var deferred = $q.defer();
+               return $http({
+                   url: 'bower_components/d3/data/suicide-squad.json', method: 'GET'})
+                   .success(function (data, status, headers, config) {
+                       deferred.resolve(data);
+                   }).error(function (data, status, headers, config) {
+                       deferred.reject(status);
+                   });
+               return deferred.promise;
+           }
+       }
+    }])
 
     .directive('importData', function(){
-        function link(scope, el){
+        function link(scope, elm, attrs ){
+
+
+            // data = JSON.stringify(data);
+            // console.log("TyTy " + data);
 
             d3.json("bower_components/d3/data/suicide-squad.json", function (data){
                 var canvas = d3.select(".dataContainer").append("svg")
@@ -51,10 +80,44 @@ angular.module('myApp.view8', ['ngRoute'])
             })
 
 
+                // var canvas = d3.select(".dataContainer").append("svg")
+                //     .attr("width", 1000)
+                //     .attr("height", 700);
+                //
+                // canvas.selectAll("rect")
+                //     .data(data)
+                //     .enter()
+                //     .append("rect")
+                //     .attr("width", function (d) {
+                //         return d.rank * 70
+                //     })
+                //     .attr("height", 50)
+                //     .attr("y", function (d, i) {
+                //         return i * 80;
+                //     })
+                //     .attr("fill", "red");
+                //
+                // canvas.selectAll("text")
+                //     .data(data)
+                //     .enter()
+                //     .append("text")
+                //     .attr("fill", "#ffffff")
+                //     .attr("y", function (d,i) {
+                //         return i * 80 + 30
+                //     })
+                //     .attr("x", 5)
+                //     .text(function (d) {
+                //         return d.name + " rank: " + d.rank;
+                //     })
+
+
+
         }
         return {
-            link: link,
+
             restrict: 'E',
-            scope: { data: '=' }
+            scope: { data: '=' },
+            controller: "View8Ctrl",
+            link: link
         }
     });
--- app/view9/view9.js
+++ app/view9/view9.js
@@ -7,11 +7,120 @@ angular.module('myApp.view9', ['ngRoute'])
         });
     }])
 
-    .controller('View9Ctrl', [function() {
+    .controller('View9Ctrl', ['$scope', function($scope) {
+
+        $scope.answer = {
+            "path": true,
+            "arc":  true
+        }
+
+        $scope.changeToPath = function(){
+            var canvas = d3.select(".paths").selectAll("svg");
+
+            var data = [
+                {x: 10, y: 20},
+                {x: 100, y: 100},
+                {x: 10, y: 200},
+                {x: 30, y: 80},
+                {x: 60, y: 200}
+            ];
+
+            canvas.selectAll("g").remove();
+
+            var group = canvas.append("g")
+                .attr("transform", "translate(100,100)");
+
+            var line = d3.line()
+                .x(function (d) {
+                    return d.x
+                })
+                .y(function (d) {
+                    return d.y
+                });
+
+
+            group.selectAll("path")
+                .data([data])
+                .enter()
+                .append("path")
+                .attr("d", line)
+                .attr("fill", "none")
+                .attr("stroke", "red")
+                .attr("stroke-width", 5);
+        }
+
+        $scope.changeToArc = function () {
+
+            var canvas = d3.select(".paths").selectAll("svg");
+
+            canvas.selectAll("g").remove();
+
+            var group = canvas.append("g")
+                .attr("transform", "translate(100,100)");
+
+            var radius = 50;
+            var p = Math.PI * 2;
+
+            var arc = d3.arc()
+                .innerRadius(radius - 10)
+                .outerRadius(radius)
+                .startAngle(0)
+                .endAngle(p)
+
+            group.append("path")
+                .attr("d", arc);
+        }
+
+        $scope.changeToDefault = function () {
+
+            var canvas = d3.select(".paths").selectAll("svg");
+
+            canvas.selectAll("g").remove();
+
+            var data = [
+                {x: 10, y: 20},
+                {x: 100, y: 100},
+                {x: 10, y: 200},
+                {x: 30, y: 80},
+                {x: 60, y: 200}
+            ];
+
+            var group = canvas.append("g")
+                .attr("transform", "translate(100,100)");
+
+            var line = d3.line()
+                .x(function (d) {
+                    return d.x
+                })
+                .y(function (d) {
+                    return d.y
+                });
+
+            group.selectAll("path")
+                .data([data])
+                .enter()
+                .append("path")
+                .attr("d", line)
+                .attr("fill", "none")
+                .attr("stroke", "red")
+                .attr("stroke-width", 5);
+
+            var radius = 50;
+            var p = Math.PI * 2;
+
+            var arc = d3.arc()
+                .innerRadius(radius - 10)
+                .outerRadius(radius)
+                .startAngle(0)
+                .endAngle(p)
+
+            group.append("path")
+                .attr("d", arc);
+        }
 
     }])
 
-    .directive('paths', function(){
+    .directive('paths', function(){ //Practice Code to work on paths
         function link(scope, el){
 
             var canvas = d3.select(".paths").append("svg")
@@ -21,7 +130,9 @@ angular.module('myApp.view9', ['ngRoute'])
             var data = [
                 {x: 10, y: 20},
                 {x: 100, y: 100},
-                {x: 10, y: 200}
+                {x: 10, y: 200},
+                {x: 30, y: 80},
+                {x: 60, y: 200}
             ];
 
             var group = canvas.append("g")
@@ -47,12 +158,12 @@ angular.module('myApp.view9', ['ngRoute'])
         }
         return {
             link: link,
-            restrict: 'E',
+            restrict: 'EA',
             scope: { data: '=' }
         }
     })
 
-    .directive('arcs', function(){
+    .directive('arcs', function(){ //Practice Code to work on Arcs
         function link(scope, el){
 
             var canvas = d3.select(".paths").append("svg")
@@ -62,7 +173,9 @@ angular.module('myApp.view9', ['ngRoute'])
             var data = [
                 {x: 10, y: 20},
                 {x: 100, y: 100},
-                {x: 10, y: 200}
+                {x: 10, y: 200},
+                {x: 30, y: 80},
+                {x: 60, y: 200}
             ];
 
             var group = canvas.append("g")
@@ -102,7 +215,8 @@ angular.module('myApp.view9', ['ngRoute'])
         return {
             link: link,
             restrict: 'E',
-            scope: { data: '=' }
+            scope: { data: '=' },
+            controller: "View9Ctrl"
         }
     });
 
